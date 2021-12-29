c7e0b38e76bde262e4d95aac49e7310f4eee9970
Bryan Jonas <bryan@crashbang.dev>
2021-12-28 18:55:52 -0600

    Update startup.sh

--- vipr/startup.sh
+++ vipr/startup.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-python setup.py install
+apt update
+apt install -y --no-install-recommends librtlsdr-dev
+cd /system
+pip install .[with_pymodes]
 adsbcot -U tcp:tak:8087 -D tcp+raw:adsb:30002
