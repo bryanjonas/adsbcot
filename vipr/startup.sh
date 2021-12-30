e6a18d8049bbefb916c4a028dbd2eadb08c446ed
Bryan Jonas <bryan@crashbang.dev>
2021-12-30 13:43:41 -0600

    Update config, input, startup

--- vipr/startup.sh
+++ vipr/startup.sh
@@ -3,5 +3,5 @@
 apt update
 apt install -y --no-install-recommends librtlsdr-dev
 cd /system
-pip install .[with_pymodes]
+/usr/local/bin/pip install .[with_pymodes] --use-feature=in-tree-build
 adsbcot -U tcp:tak:8087 -D tcp+raw:adsb:30002
